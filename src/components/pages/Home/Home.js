f8b36013ee4ec1054ba377f97467f928317d8182
Junny <tajo0215@gmail.com>
2021-08-16 19:28:36 -0700

    Home and Menu Components added

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div class="section-center">
+      <h1 class="mb-0">Pure CSS Menu</h1>
+    </div>
+  );
+}
+
+export default Home;
