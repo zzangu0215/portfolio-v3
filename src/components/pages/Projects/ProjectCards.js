41cab27a4de3f05e31874a95be5c64760cc32e00
Junny <tajo0215@gmail.com>
2021-08-18 09:27:32 -0700

    project cards bug fixed

--- src/components/pages/Projects/ProjectCards.js
+++ src/components/pages/Projects/ProjectCards.js
@@ -7,20 +7,18 @@ function ProjectCards(props) {
   };
 
   return (
-    <div className="cards-wrapper">
-      <div className="card-grid-space" style={cardBgImg}>
-        <div className="card">
-          <div>
-            <a href={props.deployed_link}>
-              <h1>{props.name}</h1>
-            </a>
-            <p>{props.description}</p>
-            <a href={props.github_link}>
-              <div className="date">Github</div>
-            </a>
-            <div className="tags">
-              <div className="tag">HTML</div>
-            </div>
+    <div className="card-grid-space" style={cardBgImg}>
+      <div className="card">
+        <div>
+          <a href={props.deployed_link}>
+            <h1>{props.name}</h1>
+          </a>
+          <p>{props.description}</p>
+          <a href={props.github_link}>
+            <div className="date">Github</div>
+          </a>
+          <div className="tags">
+            <div className="tag">HTML</div>
           </div>
         </div>
       </div>
--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -9,18 +9,20 @@ function Projects() {
     <div>
       <h1 className="projects">Projects</h1>
       <div className="row projects">
-        {projectLists.map((project) => (
-          <ProjectCards
-            key={project.id}
-            name={project.name}
-            isTeamProject={project.isTeamProject}
-            thumbnail={project.thumbnail}
-            description={project.description}
-            github_link={project.github_link}
-            deployed_link={project.deployed_link}
-            technologies={project.technologies}
-          />
-        ))}
+        <div className="cards-wrapper">
+          {projectLists.map((project) => (
+            <ProjectCards
+              key={project.id}
+              name={project.name}
+              isTeamProject={project.isTeamProject}
+              thumbnail={project.thumbnail}
+              description={project.description}
+              github_link={project.github_link}
+              deployed_link={project.deployed_link}
+              technologies={project.technologies}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
