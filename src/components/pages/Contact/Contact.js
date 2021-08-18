b3ed6e3490bda4ee98e3f1b91323e18d21d99722
Junny <tajo0215@gmail.com>
2021-08-18 03:11:09 -0700

    form is connected to emailjs

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,15 +1,43 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import "./Contact.css";
 
 function Contact() {
+  const { register, handleSubmit, watch } = useForm();
+  const onSubmit = (data) => console.log(data);
+  const message = watch("message") || "";
+  const messageCharsLeft = 2000 - message.length;
+
   return (
     <div>
       <h1 className="contact-me">Contact Me</h1>
-      <form id="form" className="topBefore">
-        <input id="name" type="text" placeholder="NAME" />
-        <input id="email" type="text" placeholder="E-MAIL" />
-        <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
-        <input id="submit" type="submit" value="GO!" />
+      <form id="form" className="topBefore" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          {...register("name", { required: true })}
+          maxLength="30"
+          placeholder="NAME"
+        />
+        <input
+          id="email"
+          name="email"
+          type="email"
+          {...register("email", { required: true })}
+          placeholder="E-MAIL"
+        />
+        <textarea
+          id="message"
+          name="message"
+          type="text"
+          {...register("message", { required: true })}
+          maxLength="2000"
+          placeholder="MESSAGE"
+        ></textarea>
+        <p className="message-chars-left">{messageCharsLeft}</p>
+        <br />
+        <input id="submit" type="submit" value="Send" />
       </form>
     </div>
   );
