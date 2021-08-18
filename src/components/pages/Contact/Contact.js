c7dffd595398d51904f14f2391d7aefe1ab941c9
Junny <tajo0215@gmail.com>
2021-08-18 03:28:51 -0700

    Contact Form completed

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { init, sendForm } from "emailjs-com";
 import "./Contact.css";
 
+init("user_ACIlhK9PWLR24gNpYF5wC");
+
 function Contact() {
+  const [contactNumber, setContactNumber] = useState("000000");
+  const [statusMessage, setStatusMessage] = useState("Message");
+
+  const generateContactNumber = () => {
+    const numStr = "000000" + ((Math.random() * 1000000) | 0);
+    setContactNumber(numStr.substring(numStr.length - 6));
+  };
+
   const { register, handleSubmit, watch } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    const form = document.querySelector("#form");
+    const statusMessage = document.querySelector(".status-message");
+
+    generateContactNumber();
+    sendForm("default_service", "template_h9d7i89", "#form").then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        form.reset();
+        setStatusMessage("Message sent!");
+        statusMessage.className = "status-message success";
+        setTimeout(() => {
+          statusMessage.className = "status-message";
+        }, 5000);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        setStatusMessage("Failed to send message! Please try again later.");
+        statusMessage.className = "status-message failure";
+        setTimeout(() => {
+          statusMessage.className = "status-message";
+        }, 5000);
+      }
+    );
+    // console.log(data)
+  };
   const message = watch("message") || "";
   const messageCharsLeft = 2000 - message.length;
 
   return (
     <div>
       <h1 className="contact-me">Contact Me</h1>
+      <p className="status-message">{statusMessage}</p>
       <form id="form" className="topBefore" onSubmit={handleSubmit(onSubmit)}>
+        <input type="hidden" name="contact_number" value={contactNumber} />
         <input
           id="name"
           name="name"
